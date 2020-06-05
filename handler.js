80e23eea05cdf29514da383d9e6fa172bcb4819d
Defiance99 <56317348+Defiance99@users.noreply.github.com>
2020-06-05 20:27:26 +0300

    Add files via upload

--- handler.js
+++ handler.js
@@ -1,31 +1,52 @@
-var calculatorValues = document.getElementById("CalculatorConteiner").getElementsByTagName("td");
+let calculatorValues = document.getElementById("CalculatorConteiner").getElementsByTagName("tr");
+console.log(calculatorValues);
 let expression = "";
+let pass = true; // in a string must be only one operator
+let operators = ["/","*","+","-"];
+
 
 addActForEachElement(calculatorValues);
 
 
 function addActForEachElement(calculatorValues) {
     for (let i = 0; i < calculatorValues.length; i++) {
-        calculatorValues[i].addEventListener("mouseup", () => {
-            let simbol = calculatorValues[i].textContent;
+        calculatorValues[i].addEventListener("mouseup", (element) => {
+            let simbol = element.target.textContent;
             actProcessing(simbol);
         })
     }
-}
+} 
+
 
 function actProcessing(simbol) {
     if (simbol == "Clear") {
         expression = "";
-        displayingexpression(expression);
+        pass = true;
+        displayingExpression(expression);
         return;
     }
     if (simbol == "C") {
+        if (operators.includes(expression.slice(-1))) pass = true;
         expression = expression.slice(0,expression.length - 1);
-        displayingexpression(expression);
+        displayingExpression(expression);
         return;
     }
-    if (simbol == "/" || simbol == "*" || simbol == "+" || simbol == "-") {
-        if (/\/|\+|\-|\*/.test(expression.slice(-1))) return; //checking two operators in a row
+    if (simbol == "-" && expression.length == 0) {
+        expression += simbol;
+        displayingExpression(expression);
+        return;
+    }
+    if (operators.includes(simbol)) {
+        if (!pass && operators.includes(expression.slice(-1))) {
+            expression = expression.slice(0,expression.length - 1) + simbol;
+            pass = false;
+            displayingExpression(expression);
+            return;
+        }
+        if (pass) {
+            pass = false;
+        }
+        else return;
     }
 
     if (simbol == "=") {
@@ -35,34 +56,29 @@ function actProcessing(simbol) {
 
     if (expression.length > 21) return;
     expression = expression + simbol;
-    displayingexpression(expression);
+    displayingExpression(expression);
 }
 
 
 function executeExpression(exp) {
-    let operatorsArr = exp.split(/[+-/*]/).filter(el => el != ""); 
-    let valueArr = exp.split(/[^-+/*]/).filter(el => el != "");
-    console.log(operatorsArr);
-    console.log(valueArr);
     let resultOfExp = 0;
-    
-    for (let i = 0; i < valueArr.length; i++) {
-        if (valueArr[i] == "+") {
-            resultOfExp = +operatorsArr[0] + +operatorsArr[1];
-            operatorsArr.shift();
-            operatorsArr[0] = resultOfExp;
-        }
-        if (valueArr[i] == "-") {
-            resultOfExp = +operatorsArr[0] - +operatorsArr[1];
-            operatorsArr.shift();
-            operatorsArr[0] = resultOfExp;
-        }
+
+    for (let i = 1; i < exp.length; i++) {
+        if (exp[i] == "*") resultOfExp = +exp.slice(0,i) * +exp.slice(i + 1);
+        if (exp[i] == "/") resultOfExp = +exp.slice(0,i) / +exp.slice(i + 1);
+        if (exp[i] == "+") resultOfExp = +exp.slice(0,i) + +exp.slice(i + 1);
+        if (exp[i] == "-") resultOfExp = +exp.slice(0,i) - +exp.slice(i + 1);
     }
 
-    displayingexpression(resultOfExp);
+    console.log(exp,resultOfExp);
+
+    pass = true;
+    expression = String(resultOfExp);
+    displayingExpression(resultOfExp);
 }
 
-function displayingexpression(expression) {
+
+function displayingExpression(expression) {
     let displayBar = document.getElementById("js-display");
     displayBar.textContent = expression;
 }
